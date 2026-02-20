90d46d013efea92c6777fac8a67d93c946b369f2
Tasnim Ahmed <tasnim@tazzix.com>
2026-02-20 17:10:06 +0500

    Add src/entities/Comment.ts

---
+++ src/entities/Comment.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne } from 'typeorm';
+import { User } from './User';
+import { Post } from './Post';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User, (user) => user.comments, { onDelete: 'CASCADE' })
+  author: User;
+
+  @ManyToOne(() => Post, (post) => post.comments, { onDelete: 'CASCADE' })
+  post: Post;
+}
